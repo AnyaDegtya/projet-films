860bb0bf04d3046a3162face5d56c5bfbd8d0dae
Hanna <naana1999@mail.ru>
2023-06-02 16:36:31 +0200

    new

---
+++ js/film.js
@@ -0,0 +1,18 @@
+const numberOfFilms = "Сколько фильмов вы уже посмотрели?"
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        rating:
+
+
+    }
+    actors: {
+
+    }
+    genres: [],
+    privat: false
+}
+
+const cvastion1 = prompt("Один из последних просмотренных фильмов?",);
+const cvastion2 = prompt("На сколько оцените его?", "");
