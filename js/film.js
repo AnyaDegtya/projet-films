6fe8a011b60637a4aa8f5d75b54f1fc7621932fb
Hanna <naana1999@mail.ru>
2023-06-02 17:31:02 +0200

    added user questions

--- js/film.js
+++ js/film.js
@@ -1,18 +1,18 @@
-const numberOfFilms = "Сколько фильмов вы уже посмотрели?"
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ")
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        rating:
-
-
-    }
-    actors: {
-
-    }
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
-}
+};
 
-const cvastion1 = prompt("Один из последних просмотренных фильмов?",);
-const cvastion2 = prompt("На сколько оцените его?", "");
+const a = prompt("Один из последних просмотренных фильмов?", " "),
+    b = prompt("На сколько оцените его?", " "),
+    c = prompt("Один из последних просмотренных фильмов?", " "),
+    d = prompt("На сколько оцените его?", " ");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
